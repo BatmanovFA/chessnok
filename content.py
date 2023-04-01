7b4eea530a583bdd38df027aed4d7be7e4b9ac6e
igorbat99 <igorbat99@gmail.com>
2023-04-01 15:31:16 +0500

    Initial commit

---
+++ content.py
@@ -0,0 +1,25 @@
+DECK = [
+    "Pawn",
+    "Pawn",
+    "Pawn",
+    "Pawn",
+    "Pawn",
+    "Pawn",
+    "Pawn",
+    "Pawn",
+    "Knight",
+    "Knight",
+    "Bishop",
+    "Bishop",
+    "Rook",
+    "Rook",
+    "Queen"
+]
+
+FIGURE = [
+    "Pawn",
+    "Knight",
+    "Bishop",
+    "Rook",
+    "Queen"
+]
---
+++ telegram_bot.py
@@ -0,0 +1,24 @@
+# Подключение библиотек стандартных
+import telebot
+# from telebot import types
+
+# Подключение собственно созданных библиотек
+# import Chessnok_alpha
+
+# Подключение к ботам
+Telegram_bot = telebot.TeleBot('6135051270:AAFM3_Fx-PrQBGIXxVrJS6Fkq4Kd-phCiAE')
+
+# Получение и обработка текствоых сообщений
+@Telegram_bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Hi":
+        Telegram_bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    else:
+        Telegram_bot.send_message(message.from_user.id, "G")
+
+# Работа бота
+def main():
+    Telegram_bot.infinity_polling()
+
+if __name__ == "__main__":
+    main()
